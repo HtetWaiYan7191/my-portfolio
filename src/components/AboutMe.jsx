9e80608d6137f95f6437033291bca1af747c5238
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-27 11:22:44 +0630

    Start working on About me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <section className='about-me-container h-[100vh]'>
+      <div className='about-me'>
+        <h2 className='text-sky-200 font-mono'>About Me</h2>
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe
