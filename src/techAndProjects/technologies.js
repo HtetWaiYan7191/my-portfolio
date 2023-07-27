0124175abff83a840c83943bfdceec13893b70f7
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-27 20:59:59 +0630

    Add technologies for techstacks image and name

---
+++ src/techAndProjects/technologies.js
@@ -0,0 +1,89 @@
+const technologies = [
+    {
+      stack: 'frontend',
+      name: 'HTML 5',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'CSS 3',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'JavaScript',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    },
+    {
+      stack: 'backend',
+      name: 'Ruby',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'React JS',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    },
+    {
+      stack: 'backend',
+      name: 'Ruby on Rails',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'Redux Toolkit',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'Bootstrap',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'Tailwind CSS',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+    },
+    {
+      stack: 'frontend',
+      name: 'Jest',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    },
+    {
+      stack: 'backend',
+      name: 'RSpec',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rspec/rspec-original.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'PostgreSQL',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'Git',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'GitHub',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'Webpack',
+      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-plain.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'Figma',
+      icon: 'https://www.vectorlogo.zone/logos/figma/figma-icon.svg',
+    },
+    {
+      stack: 'tools',
+      name: 'Postman',
+      icon: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
+    },
+  ];
+
+  export default technologies;
