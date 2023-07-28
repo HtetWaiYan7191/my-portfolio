61c01b4e8aa231a12a3a8aee03d1dcb059a0cf4f
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-28 15:07:32 +0630

    Add skill section to show tech stacks mobile version

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,55 +3,42 @@ import '../styles/Skills.css';
 import technologies from '../techAndProjects/technologies';
 
 const Skills = () => {
-    const [fullStack, setFullStack] = useState(true);
-    const [frontEnd, setFrontEnd] = useState(false);
-    const [backEnd, setBackEnd] = useState(false);
-    const [tools, setTools] = useState(false);
+
+   const [currentStack, setCurrentStack] = useState('fullstack');
 
     const techStacks = ['fullstack','frontend','backend','tools'];
+    const filterTechnologies = technologies.filter((tech) => tech.stack === currentStack);
+    console.log(filterTechnologies)
 
-    // const filterTechnologies = technologies.filter((tech) => tech.stack === 'frontend');
-
-    const handleFullStack = () => {
-        setFullStack(!fullStack)
-        setFrontEnd(false)
-        setBackEnd(false)
-        setTools(false)
-    }
-
-    const handleFrontEnd = () => {
-        setFullStack(false)
-        setFrontEnd(true)
-        setBackEnd(false)
-        setTools(false)
-    }
-
-    const handleBackEnd = () => {
-        setFullStack(false)
-        setFrontEnd(false)
-        setBackEnd(true)
-        setTools(false)
-    }
-
-    const handleTools = () => {
-        setFullStack(false)
-        setFrontEnd(false)
-        setBackEnd(false)
-        setTools(true)
+    const handleClick = (stack) => {
+      setCurrentStack(stack);
     }
   return (
     <section className='skill-section-container h-[100vh]'>
       <h2 className='text-sky-200 font-semibold font-mono text-center text-3xl about-me-title '>TECH STACK</h2>
       <div className='skill-section my-10 '>
            <div className='skill-button-container flex justify-evenly w-[90%] mx-auto items-center'>
-            <button type="button" className={` relative  px-3 py-2  font-semibold ${fullStack ? `bg-slate-900 text-sky-500 ` : `text-white`}`} onClick={handleFullStack}>FullStack</button>
-            <button type="button" className={` relative  px-3 py-2  font-semibold ${frontEnd ? `bg-slate-900 text-sky-500 ` : `text-white`}`} onClick={handleFrontEnd}>FrontEnd</button>
-            <button type="button" className={` relative  px-3 py-2  font-semibold ${backEnd ? `bg-slate-900 text-sky-500 ` : `text-white`}`} onClick={handleBackEnd}>BackEnd</button>
-            <button type="button" className={` relative  px-3 py-2   font-semibold ${tools ? `bg-slate-900 text-sky-500 ` : `text-white`}`} onClick={handleTools}>Tools</button>
+            {techStacks.map((stack) => (
+                <button type="button" key={stack} className={` px-3 capitalize ${currentStack === stack ? `bg-slate-900 text-sky-500` : `text-white`}  py-2  font-semibold`} onClick={() => handleClick(stack)}>{stack}</button>
+            ))}
            </div>
       </div>
-      <div className='skill-image-container'>
-
+      <div className='skill-image-container grid grid-cols-4 gap-x-5 gap-y-6'>
+              {
+                currentStack === 'fullstack' ? (
+                    technologies.map((tech) => (
+                        <figure key={tech.icon} className='border-slate-900 shadow-sky-600 hover:shadow-sky-600/50 shadow-md border rounded-br-3xl  bg-slate-900 hover:bg-slate-900/50 flex justify-center p-5'>
+                          <img src={tech.icon} className='skill-icons' alt="" />
+                        </figure>
+                    ))
+                ) : (
+                  filterTechnologies.map((tech) => (
+                  <figure className='border-slate-900 shadow-sky-600 hover:shadow-sky-600/50 shadow-md border rounded-br-3xl  bg-slate-900 hover:bg-slate-900/50 flex justify-center p-5'>
+                    <img src={tech.icon} className='' alt={tech.stack} />
+                  </figure>
+                  ))
+                )
+              }
       </div>
     </section>
   )
