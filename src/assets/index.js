ec76fed40ed7788500dd89256c14dbbc72fcf976
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-28 21:52:08 +0630

    Export required images for projects

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+//proects 
+import bookStore from './images/bookStore.png'
+import foodGallery from './images/foodGallery.png'
+import leaderBoard from './images/leaderBoard.png'
+import mathMagicians from './images/mathMagicians.png'
+import todolist from './images/todolist.png'
+import pokemon from './images/pokemon.png'
+import spaceTraveler from './images/spaceTraveler.png'
+import spaceTravelerMissions from './images/spaceTravelerMissions.png'
+import spaceTravelerProfile from './images/spaceTravelerProfile.png'
+
+export {bookStore, foodGallery, leaderBoard, mathMagicians, todolist, pokemon, spaceTraveler, spaceTravelerMissions, spaceTravelerProfile}
