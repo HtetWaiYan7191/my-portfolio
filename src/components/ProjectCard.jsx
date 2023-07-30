76060d89070ecce5cdadd4f843444ddf4e3476db
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-30 13:30:23 +0630

    Fix the showProject errors

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,15 +8,18 @@ const ProjectCard = ({ project }) => {
   const [openCard, setOpenCard] = useState(false);
   const handleClick = (e) => {
     e.preventDefault();
-    if(!openCard) {
-      e.target.parentNode.parentNode.parentNode.classList.remove('transition-show-project')
-      e.target.parentNode.parentNode.parentNode.classList.add('showProject')
-      setOpenCard(!openCard)
-    } else {
-      e.target.parentNode.parentNode.parentNode.classList.add('transition-show-project')
-      e.target.parentNode.parentNode.parentNode.classList.remove('showProject')
-      setOpenCard(!openCard)
-    }
+    setOpenCard(!openCard)
+    project.cardOpen = !openCard ;
+    console.log(project.cardOpen)
+    // if(!openCard) {
+    //   e.target.parentNode.parentNode.parentNode.classList.remove('transition-show-project')
+    //   e.target.parentNode.parentNode.parentNode.classList.add('showProject')
+    //   setOpenCard(!openCard)
+    // } else {
+    //   e.target.parentNode.parentNode.parentNode.classList.add('transition-show-project')
+    //   e.target.parentNode.parentNode.parentNode.classList.remove('showProject')
+    //   setOpenCard(!openCard)
+    // }
   }
   return (
     <div className="project-card relative w-[300px]  overflow-hidden mx-auto">
@@ -27,10 +30,10 @@ const ProjectCard = ({ project }) => {
           className="w-[100%] object-cover"
         />
       </figure>
-      <div className="absolute text-sky-500  overflow-hidden z-10 overflow-card bg-slate-900/80 backdrop-blur-sm transition-show-project left-0 w-[100%] h-[100%]">
-        <div className="project title flex justify-between px-5  items-center border-b border-b-gray-50/50 opacity-80 ">
-            <h2 className=" py-2 text-[1.5rem] font-bold text-sky-300  intro-name">{project.name}</h2>
-              {!openCard ? (
+      <div className={`absolute text-sky-500  overflow-hidden z-10 ${project.cardOpen ? `showProject bottom-[0%]` : `hideProject bottom-[-74%]`} overflow-card bg-slate-900/80 backdrop-blur-sm  left-0 w-[100%] h-[100%]`}>
+        <div className={`project title flex justify-between px-5 ${project.cardOpen ? `bg-slate-900/70 showProject` : `bg-slate-900 hideProject`} items-center border-b border-b-gray-50/50 opacity-80`}>
+            <h2 className=" py-1 text-[1.5rem] font-bold text-sky-300  intro-name">{project.name}</h2>
+              {!project.cardOpen ? (
                  <button type="button" className="up-arrow-container" onClick={handleClick}>
                   <IoIosArrowUp className={`text-lg  text-gray-50 `}/>
                   </button>
@@ -44,7 +47,7 @@ const ProjectCard = ({ project }) => {
         </div>
         <ul className="project-body flex  flex-wrap justify-evenly items-center py-5 ">
             {project.techStack.map((tech, id) => (
-              <li key={id} className="text-sm text-sky-300 px-1 intro-name  ">{tech}</li>
+              <li key={id} className="text-sm text-sky-300 px-1 intro-name font-bold  ">{tech}</li>
             ))}
         </ul>
       </div>
