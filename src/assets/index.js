801829f6eadd48da7e93d4fa8a096f06e1ab9bae
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-30 16:21:48 +0630

    Export carousel images

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,4 +9,25 @@ import spaceTraveler from './images/spaceTraveler.png'
 import spaceTravelerMissions from './images/spaceTravelerMissions.png'
 import spaceTravelerProfile from './images/spaceTravelerProfile.png'
 
-export {bookStore, foodGallery, leaderBoard, mathMagicians, todolist, pokemon, spaceTraveler, spaceTravelerMissions, spaceTravelerProfile}
+//Carousel Images 
+import mathMagiciansHome from './carouselImages/mathMagicians_home.png'
+import mathMagiciansQuote from './carouselImages/mathMagicians_quote.png'
+import mathMagicians_calculator from './carouselImages/mathMagicians_calculator.png'
+import bookStoreDark from './carouselImages/bookStoreDark.png'
+import bookStoreLight from './carouselImages/bookStoreLight.png'
+import foodGalleryDetails from './carouselImages/foodGalleryDetails.png'
+import foodGalleryHome from './carouselImages/foodGalleryHome.png'
+import mealGalleryComments from './carouselImages/mealGalleryComments.png'
+import mealGalleryHome from './carouselImages/mealGalleryHome.png'
+import pokemonComments from './carouselImages/pokemonComments.png'
+import pokemonHome from './carouselImages/pokemonHome.png'
+import pokemonReservations from './carouselImages/pokemonReservations.png'
+import spaceTravelerHome from './carouselImages/spaceTravelerHome.png'
+import spaceTravelerAbout from './carouselImages/spaceTravelerAbout.png'
+import spaceMission from './carouselImages/spaceTravelerMissions.png'
+import leaderBoardMain from './carouselImages/leaderBoardMain.png'
+import todolistHome from './carouselImages/todolistHome.png'
+
+export {bookStore, foodGallery, leaderBoard, mathMagicians, todolist, pokemon, spaceTraveler, spaceTravelerMissions, spaceTravelerProfile
+,mathMagiciansHome, mathMagiciansQuote, mathMagicians_calculator, bookStoreDark, bookStoreLight, foodGalleryDetails, foodGalleryHome, mealGalleryComments, mealGalleryHome, pokemonComments, pokemonHome, pokemonReservations
+,spaceMission, spaceTravelerAbout, spaceTravelerHome, leaderBoardMain, todolistHome}
