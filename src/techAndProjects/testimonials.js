672e2aa181826ab23e1a16cf541e822aaf72bb95
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-31 21:17:24 +0630

    Add testimonials.js file for testimonials data

---
+++ src/techAndProjects/testimonials.js
@@ -0,0 +1,55 @@
+import { ansar, ichsan, kiko, fatima, adeyemi } from "../assets"
+
+const testimonials = [
+    {
+        id: 1,
+        name: 'Batool Fatima',
+        country: 'Karāchi, Sindh, Pakistan',
+        image: fatima,
+        recommendations: "Yan is an amazing developer and partner that I've had the pleasure of working with. We met at Microverse during our standup and morning session teams. During our time together, Yan showed impressive technical skills and worked really well with the team. He has a special talent for solving complex problems and finding practical solutions. Whenever I got stuck, Yan was always there to help me out. Yan's ability to articulate ideas and concepts clearly has greatly contributed to the success of our team. I highly recommend Yan as his contributions have made a positive impact on our projects, and I'm sure he will continue to succeed in future teams.",
+        linkedIn:'https://www.linkedin.com/in/batoolfatima2135/',
+        gitHub: 'https://github.com/batoolfatima2135',
+    },
+
+    {
+        id: 2,
+        name: 'Ichsan Sandy',
+        country: 'Palembang, South Sumatra, Indonesia',
+        image: ichsan,
+        recommendations: "I worked at Yan several times. As a software engineer, Yan's diligence and work ethic have been unparalleled. He consistently goes above and beyond to ensure that all assigned tasks are completed to the highest standard and ahead of schedule. Yan has an exceptional ability to prioritize his work, allowing him to not only meet deadlines but often deliver projects well in advance. His proactive approach and time management skills have been a tremendous asset to our team, and his work has had a positive impact on our overall project timelines. Moreover, his passion for technology is evident through the numerous successful side projects he has undertaken. Yan's exceptional skills and collaborative nature make him an invaluable asset to any team",
+        linkedIn:'https://www.linkedin.com/in/ichsans/',
+        gitHub: 'https://github.com/ichsansandy',
+    },
+
+    {
+        id: 3,
+        name: 'Kirti Arora',
+        country: 'Ajmer, Rajasthan, India',
+        image: kiko,
+        recommendations: "Htet Wai Yan is a remarkable Front-End Developer, demonstrating exceptional work ethic and technical skills. We collaborated closely, and he consistently delivered high-quality projects ahead of schedule. Htet Wai excels in React and Redux, solving complex problems effectively. His positive and collaborative nature positively impacted the work environment. I highly recommend him for any front-end development role.",
+        linkedIn:'https://www.linkedin.com/in/kirti-arora-6a453813a/',
+        gitHub: '',
+    },
+
+    {
+        id: 4,
+        name: 'Ansar Ibrahim',
+        country: 'Coimbatore, Tamil Nadu, India',
+        image: ansar,
+        recommendations: "Htet Wai Yan is a highly motivated and talented software developer, known for his dedication and exceptional coding skills. His passion for technology is evident in every project he undertakes. Htet Wai Yan's ability to grasp complex concepts quickly and translate them into elegant code is truly impressive. Working with Htet Wai Yan is a pleasure, as his positive attitude and enthusiasm are contagious. He is committed to meeting project deadlines and takes pride in delivering projects that exceed expectations. Companies seeking a skilled and reliable software developer should look no further than Htet Wai Yan. He is a valuable asset to any team and will undoubtedly contribute to the success of any software development endeavor.",
+        linkedIn:'https://www.linkedin.com/in/ansar-ibrahim/',
+        gitHub: 'https://github.com/AnsarIbrahim',
+    }, 
+    
+    {
+        id: 5,
+        name: 'Adeyemi Dada',
+        country: 'Lagos State, Nigeria',
+        image: adeyemi,
+        recommendations: "I wholeheartedly recommend Htet Wai (htetwaiyan) Yan as an exceptional programmer. Working closely with him, I have been consistently impressed by his technical expertise and problem-solving skills. Htet's coding abilities are outstanding, and he shows a deep understanding of complex programming concepts. His passion for innovation drives him to continuously seek knowledge and stay updated with the latest technologies. Htet is an excellent team player, collaborating effectively and communicating ideas with clarity. His analytical mindset and attention to detail make him a valuable asset in any development project. I am confident that Htet will continue to thrive and contribute significantly to the field of programming.",
+        linkedIn:'https://www.linkedin.com/in/yemidada/',
+        gitHub: 'https://github.com/yemidada',
+    },
+]
+
+export default testimonials;
