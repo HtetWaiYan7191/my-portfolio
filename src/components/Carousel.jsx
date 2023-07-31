4de71ae73895ec87fbd8a2b670d8c613c12858fa
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-31 15:45:34 +0630

    Add carousel images in pop up

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react'
 import {BsChevronLeft, BsChevronRight} from 'react-icons/bs'
 
-const Carousel = () => {
+const Carousel = ({children:carouselImage}) => {
     const [curr, setCurr] = useState(0)
 
+    const prev = () => setCurr((curr) => (curr === 0 ? carouselImage.length - 1 : curr -1));
+
+    const next = () => setCurr((curr) => (curr === carouselImage.length - 1 ? 0 : curr + 1))
+
   return (
-    <div className=' overflow-hidden relative'>
-        <div className="flex"></div>
-      <div className="absolute inset-0 flex items-center justify-between p-4">
+    <div className=' overflow-hidden relative w-[80%] mx-auto'>
+        <div className="flex transition-transform ease-out duration-500" style = {{transform:`translateX(-${curr*100}%)`}}>{carouselImage}</div>
+      <div className="absolute inset-0 flex items-center justify-between p-2">
+      <button onClick={prev} className=' p-1 rounded-full shadow bg-gray-300/80 text-gray-800 hover:bg-gray-300'>
+        <BsChevronLeft size={15} className=''/>
+      </button>
+      <button onClick={next} className=' p-1 rounded-full shadow bg-gray-300/80 text-gray-800 hover:bg-gray-300'>
+        <BsChevronRight size={15} className=''/>
+      </button>
       </div>
-      <button className='p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white'>
-        <BsChevronLeft className='text-2xl'/>
-      </button>
-      <button className='p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white'>
-        <BsChevronRight className='text-2xl'/>
-      </button>
+
+      <div className='absolute bottom-3 right-0 left-0'>
+        <div className='flex items-center justify-center gap-2'>
+            {carouselImage.map((_, i) => (
+              <div key={i} className={`transition-all duration-500 w-2 h-2 bg-slate-900  rounded-full
+                  ${curr === i ? "p-1 bg-sky-500" : "bg-opacity-50"} `} />
+            ))}
+        </div>
+      </div>
     </div>
   )
 }
