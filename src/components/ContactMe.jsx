67dc101f173377084d004c99e900fa471fb6741e
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-01 11:31:30 +0630

    Add animation in ContactMe section form

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -31,7 +31,9 @@ const ContactMe = () => {
             to assist you in any way I can.
           </p>
         </div>
-        <form className='form-container backdrop-blur-[sm]  bg-slate-900  py-10 px-5 rounded-md mx-auto '>
+        <div className='box'>
+          <span className='borderLine'></span>
+        <form className='form-container bg-slate-900  z-20  py-10 px-5 rounded-md mx-auto '>
           <h2 className='text-white/90 text-center text-2xl'>Keep In Touch</h2>
           <div className={`input-box my-7 relative w-[100%] mx-auto `}>
             <input type='text' className='w-[100%]' required value={name} onChange={handleNameChange} />
@@ -39,31 +41,32 @@ const ContactMe = () => {
           </div>
 
           <div className={`input-box my-7 relative w-[100%] mx-auto `}>
-            <input type='email' className='w-[100%]' required value={email} onChange={handleEmailChange} />
+            <input type='text' className='w-[100%]' required value={email} onChange={handleEmailChange} />
             <span>Email</span>
           </div>
 
           <div
-            className={`input-box my-7 relative w-[100%] mx-auto`}
+            className={`input-box  my-7 relative w-[100%] mx-auto`}
           >
             <textarea
               name='message'
               id='message'
-              cols='30'
-              rows='6'
+              cols='38'
+              rows='7'
               value={message}
               onChange={handleMessageChange}
               required
             ></textarea>
             <span className={` ${message ? 'hide-placeholder' : ''}`}>How can I help you?</span>
           </div>
-
          <div className="button-container flex justify-center">
          <button className="border-2 border-sky-500 bg-sky-500/20 text-sky-300 font-bold py-1 px-2  rounded-sm" type="submit" >
            Submit
         </button>
          </div>
         </form>
+        </div>
+
       </div>
     </section>
   );
