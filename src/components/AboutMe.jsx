0b0deebfd18ca680caddb1cb5bc906671965f170
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-01 16:33:27 +0630

    Add style and hover button effects for about me buttons

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -30,14 +30,14 @@ const AboutMe = () => {
   // }, []);
 
   return (
-    <section className="about-me-container h-[60vh]" id="about-me-section">
+    <section className="about-me-container h-[60vh] md:h-[90vh] " id="about-me-section">
       <h2
         className={`text-sky-200 font-semibold font-mono text-center text-3xl about-me-title transition-all duration-700 ease 
          `}
       >
         About Me
       </h2>
-      <div className="about-me-card p-5 mt-5">
+      <div className="about-me-card p-5 mt-5 md:w-[50%] mx-auto md:border md:border-cyan-400 md:p-10 md:rounded-md md:mt-10 md:transition-all duration-200 ease md:bg-slate-900 md:hover:shadow-lg md:hover:shadow-sky-500">
         <p className={`text-justify text-gray-300 para-1 transition-all duration-700 ease `}>
           I am a full-stack developer with expertise in{" "}
           <span className="text-effect">React, Redux, and Rails </span>to create
@@ -50,7 +50,7 @@ const AboutMe = () => {
         </p>
 
         <button
-          className={`border-2 border-sky-500 bg-sky-500/20 text-sky-300 font-bold py-1 px-2 mt-5 rounded-sm `}
+          className={`border-2 border-white/90 hover:border-sky-500 bg-slate-900 text-white/90 font-bold py-1 px-2 mt-5 rounded-sm d-button `}
           type="button"
         >
           Resume
