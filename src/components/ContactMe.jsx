e8d9308d04a43bbafe8e10df3025b8f1feaf9c07
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-02 12:18:34 +0630

    Implement desktop version styles

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -19,21 +19,21 @@ const ContactMe = () => {
   };
 
   return (
-    <section className='contact-me-section h-[100vh]' id='contact-me-section'>
+    <section className='contact-me-section h-[100vh] md:h-[100vh] ' id='contact-me-section'>
       <h2 className='text-sky-200 font-semibold font-mono text-center text-3xl about-me-title '>
         Contact Me
       </h2>
-      <div className='contact-me my-10 flex flex-col justify-around'>
-        <div className='ending-text  '>
-          <p className='text-white/90 text-sm text-center font-semibold my-10'>
+      <div className='contact-me my-10 flex flex-col md:flex-row md:items-center md:w-[90%] md:mx-auto justify-around'>
+        <div className='ending-text mx-auto  md:flex-1 md:px-20 '>
+          <p className='text-white/90 text-sm text-center font-semibold my-10  md:text-lg  md:my-5'>
             I am continuously interested in learning about fresh projects and exploring potential
-            collaborations. Please don't hesitate to reach out and start a conversation. I'm here
+            collaborations. So don't hesitate to reach out and start a conversation. I'm here
             to assist you in any way I can.
           </p>
         </div>
-        <div className='box'>
+        <div className='box  md:my-5 md:flex-1 mx-auto md:mx-0'>
           <span className='borderLine'></span>
-        <form className='form-container bg-slate-900  z-20  py-10 px-5 rounded-md mx-auto '>
+        <form className='form-container bg-slate-900  z-20  py-10  px-5 rounded-md mx-auto '>
           <h2 className='text-white/90 text-center text-2xl'>Keep In Touch</h2>
           <div className={`input-box my-7 relative w-[100%] mx-auto `}>
             <input type='text' className='w-[100%]' required value={name} onChange={handleNameChange} />
@@ -53,14 +53,15 @@ const ContactMe = () => {
               id='message'
               cols='38'
               rows='7'
+              className='md:w-[100%]'
               value={message}
               onChange={handleMessageChange}
               required
             ></textarea>
             <span className={` ${message ? 'hide-placeholder' : ''}`}>How can I help you?</span>
           </div>
-         <div className="button-container flex justify-center">
-         <button className="border-2 border-sky-500 bg-sky-500/20 text-sky-300 font-bold py-1 px-2  rounded-sm" type="submit" >
+         <div className="button-container flex justify-center md:my-5">
+         <button className="border-2 d-button border-sky-500 bg-sky-500/20 text-sky-300 font-bold py-1 px-2  rounded-sm" type="submit" >
            Submit
         </button>
          </div>
