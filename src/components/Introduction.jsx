b3a8cc5f7de47ae0f5d19910f853def9db8fea4f
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-03 21:56:49 +0630

    Add motion.div framer motion in introduction section

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,16 +6,43 @@ import {BsInstagram} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs';
 import {FaAngellist} from 'react-icons/fa';
 import { BsFacebook } from 'react-icons/bs';
+import { motion } from "framer-motion"
 
 const Introduction = () => {
   return (
     <section className='intro-section-container flex-col  flex justify-around h-[100vh] items-center ' id='introduction-section'>
       <div className='intro-section flex flex-col items-center'>
-          <figure className={`intro-img-container `}>
+          <motion.div variants={{
+            hidden: {opacity:0, y:-75},
+            visible: {opacity: 1, y:0},
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{duration:1}}
+    ><figure className={`intro-img-container `}>
               <img src={programming} alt="intro-image" />
           </figure>
-          <h2 className='text-sky-500 font-extrabold text-3xl mt-4 font-mono intro-name'>Htet Wai Yan</h2>
+          </motion.div>
+      <motion.div variants={{
+        hidden: {opacity:0, x:-70},
+        visible: {opacity: 1 , x:0}
+      }} 
+      initial="hidden"
+      animate="visible"
+      transition={{duration: 0.5}}
+      >
+        <h2 className='text-sky-500 font-extrabold text-3xl mt-4 font-mono intro-name'>Htet Wai Yan</h2>
       <h3 className='text-sky-200 font-mono mt-3'>Full Stack Developer</h3>
+      </motion.div>
+
+      <motion.div variants={{
+        hidden: {opacity:0, x:-50},
+        visible: {opacity: 1 , x:0}
+      }} 
+      initial="hidden"
+      animate="visible"
+      transition={{duration: 0.5}}
+      >
       <ul className='social-links flex mt-6 justify-center items-center'>
         <li className='me-5 text-center px-2 hover:text-sky-500 text-sky-200 text-medium'><a href='https://www.linkedin.com/in/htet-wai-yan19/' target='blank'><BsLinkedin/></a ></li>
         <li className='me-5 text-center px-2 hover:text-sky-500 text-sky-200 text-medium'><a href='https://www.instagram.com/1918hwy/' target='blank'><BsInstagram/></a ></li>
@@ -23,11 +50,13 @@ const Introduction = () => {
         <li className='me-5 text-center px-2 hover:text-sky-500 text-sky-200 text-medium'><a href='https://wellfound.com/u/htet-wai-yan-1' target='blank'><FaAngellist/></a ></li>
         <li className='me-5 text-center px-2 hover:text-sky-500 text-sky-200 text-medium'><a href='https://www.facebook.com/profile.php?id=100010329042751' target='blank'><BsFacebook/></a></li>
       </ul>
+      </motion.div>
       </div>
+    
 
       <div className='scrolldown-container flex flex-col'>
             <span className='text-sm text-sky-200 font-mono'>Scroll Down</span>
-             <a href='' target=''> 
+             <a href='#about-me-section' target=''> 
              <div className="mouse-container">
               <div className='mouse-wheel'></div>
             </div>
