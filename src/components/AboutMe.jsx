1d8387559a981d703a38d58e840d1157345713eb
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-08-03 22:20:41 +0630

    Add scroll animation effect in about section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,20 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import React from "react";
 import "../styles/AboutMe.css";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 const AboutMe = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, {once:false});
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if(isInView) {
+      controls.start('visible')
+    }
+  } ,[isInView])
+
   return (
     <section
       className="about-me-container h-[60vh] md:h-[80vh] lg:h-[100vh] md:pt-36 pt-32 "
       id="about-me-section"
     >
+      <motion.div
+      variants={{
+        hidden: {opacity:0, x:-55},
+        visible: {opacity:1, x:0}
+      }}
+      initial="hidden"
+      animate={controls}
+      transition={{duration: 0.5}}
+      ref={ref}>
       <h2
         className={`text-sky-200 font-semibold font-mono text-center text-3xl about-me-title transition-all duration-700 ease 
          `}
       >
         About Me
       </h2>
-      <div className="about-me-card p-5 mt-5 md:w-[50%] mx-auto md:border md:border-cyan-400 md:p-10 md:rounded-md md:mt-10 md:transition-all duration-200 ease md:bg-slate-900 md:hover:shadow-lg md:hover:shadow-sky-500">
+      </motion.div>
+      <motion.div variants={{
+        hidden: {opacity:0, y:75},
+        visible: {opacity:1, y:0}
+      }}
+      initial="hidden"
+      animate={controls}
+      transition={{duration: 0.5}}
+      ref={ref}
+      
+      >
+      <div  className="about-me-card p-5 mt-5 md:w-[50%] mx-auto md:border md:border-cyan-400 md:p-10 md:rounded-md md:mt-10 md:transition-all duration-200 ease md:bg-slate-900 md:hover:shadow-lg md:hover:shadow-sky-500">
         <p
           className={`text-justify text-gray-300 para-1 transition-all duration-700 ease `}
         >
@@ -38,6 +69,7 @@ const AboutMe = () => {
           </button>
         </a>
       </div>
+      </motion.div>
     </section>
   );
 };
