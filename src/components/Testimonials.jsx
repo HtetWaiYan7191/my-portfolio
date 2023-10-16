44c6dfb860c585e04e2c0b4e756e67a21bae60cb
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-16 21:30:13 +0630

    Fix scroll error in mobile in testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,6 +10,7 @@ const Testimonials = () => {
   const testimonialRefInView = useInView(testimonialRef, {once:false});
   const headerControls = useAnimation();
   const testimonialControls = useAnimation();
+  const isDesktop = window.innerWidth > 768; 
 
   useEffect(() => {
     if(headerRefInView) {
@@ -23,30 +24,35 @@ const Testimonials = () => {
   return (
     <section className='testimonials-container md:h-[200vh] lg:h-[150vh] md:pt-36 pt-32 ' id='testimonial-section'>
       <motion.div
-      variants={{
-        hidden: { opacity: 0, y: 75 },
-        visible: { opacity: 1, y: 0 },
-      }}
-      initial='hidden'
-      animate={headerControls}
-      transition={{ duration: 0.5 }}
-      ref={headerRef}>
+  variants={{
+    hidden: { opacity: 0, y: -75 },
+    visible: { opacity: 1, y: 0, transition: { delay: 0.3, duration: 0.5 } },
+  }}
+  initial="hidden"
+  animate={headerControls}
+  transition={{ duration: 0.5 }}
+  ref={headerRef}
+>
         <h2 className='text-sky-200 font-semibold font-mono text-center text-3xl about-me-title '>Testimonials</h2>
         </motion.div>
-      <div className='testimonial-card-container  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:w-[90%] md:mx-auto gap-10 mt-10'>
-        {testimonials.map((testimonial,id) => (
-            <motion.div key={id}
+
+        <motion.div 
             variants={{
               hidden: {opacity:0, x:-50},
-              visible: {opacity:1, x:0, transition:{delay: id * 0.1, duration: 0.6}},
+              visible: {opacity:1, x:0},
             }}
             initial="hidden"
             animate={testimonialControls}
+            transition={{ duration: 0.5}}
             ref={testimonialRef}>
+      <div className='testimonial-card-container  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:w-[90%] md:mx-auto gap-10 mt-10'>
+        {testimonials.map((testimonial) => (
+            
             <TestimonialCard key={testimonial.id} testimonial={testimonial}/>
-            </motion.div>
+         
         ))}
       </div>
+      </motion.div>
     </section>
   )
 }
