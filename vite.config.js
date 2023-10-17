beee6ed4fb7fc56ea437deaa7eb09111d21fb219
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-18 01:20:34 +0630

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    rollupOptions: {
+      external: ['@popperjs/core'],
+    },
   },
 })
