7ab38069faaaa9c7c858e6f6bf5ee082b2ee4e70
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-03 21:39:34 +0630

    Add the index in technologies

--- src/techAndProjects/technologies.js
+++ src/techAndProjects/technologies.js
@@ -1,99 +1,118 @@
 const technologies = [
-    {
-      stack: 'frontend',
-      name: 'HTML 5',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'CSS 3',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'JavaScript',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
-    },
-    {
-      stack: 'backend',
-      name: 'Ruby',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'MySQL',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg',
-    },
-    {
-      stack: 'backend',
-      name: 'Php',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg',
-    },
-    {
-      stack: 'backend',
-      name: 'Laravel',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'React JS',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-    },
-    {
-      stack: 'backend',
-      name: 'Ruby on Rails',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'Redux Toolkit',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'Bootstrap',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'Tailwind CSS',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
-    },
-    {
-      stack: 'frontend',
-      name: 'Jest',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
-    },
-    {
-      stack: 'backend',
-      name: 'RSpec',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rspec/rspec-original.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'PostgreSQL',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'Git',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'GitHub',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'Webpack',
-      icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-plain.svg',
-    },
-    {
-      stack: 'tools',
-      name: 'Figma',
-      icon: 'https://www.vectorlogo.zone/logos/figma/figma-icon.svg',
-    },
-  ];
+  {
+    id: 1,
+    stack: "frontend",
+    name: "HTML 5",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+  },
+  {
+    id: 2,
+    stack: "frontend",
+    name: "CSS 3",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+  },
+  {
+    id: 3,
+    stack: "frontend",
+    name: "JavaScript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+  },
+  {
+    id: 4,
+    stack: "backend",
+    name: "Ruby",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain.svg",
+  },
+  {
+    id: 5,
+    stack: "tools",
+    name: "MySQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg",
+  },
+  {
+    id: 6,
+    stack: "backend",
+    name: "Php",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg",
+  },
+  {
+    id: 7,
+    stack: "backend",
+    name: "Laravel",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
+  },
+  {
+    id: 8,
+    stack: "frontend",
+    name: "React JS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    id: 9,
+    stack: "backend",
+    name: "Ruby on Rails",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg",
+  },
+  {
+    id: 10,
+    stack: "frontend",
+    name: "Redux Toolkit",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+  },
+  {
+    id: 11,
+    stack: "frontend",
+    name: "Bootstrap",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+  },
+  {
+    id: 12,
+    stack: "frontend",
+    name: "Tailwind CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+  },
+  {
+    id: 13,
+    stack: "frontend",
+    name: "Jest",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+  },
+  {
+    id: 14,
+    stack: "backend",
+    name: "RSpec",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rspec/rspec-original.svg",
+  },
+  {
+    id: 15,
+    stack: "tools",
+    name: "PostgreSQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg",
+  },
+  {
+    id: 16,
+    stack: "tools",
+    name: "Git",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+  },
+  {
+    id: 17,
+    stack: "tools",
+    name: "GitHub",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+  },
+  {
+    id: 18,
+    stack: "tools",
+    name: "Webpack",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-plain.svg",
+  },
+  {
+    id: 19,
+    stack: "tools",
+    name: "Figma",
+    icon: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+  },
+];
 
-  export default technologies;
+export default technologies;
