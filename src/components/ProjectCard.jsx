2475ee49ae450ec2e2142c46f686d791a4ee2803
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-12-04 15:37:10 +0630

    Change the text size and fix background color error in project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -36,8 +36,8 @@ const ProjectCard = ({ project, handlePopUp, openPopCard }) => {
         />
       </figure>
       <div className={`absolute text-sky-500  ${isHovered ? `showProject md:bottom-[0%] md:block` : `hideProject md:bottom-[-100%] `}  overflow-hidden z-10 ${project.cardOpen ? `showProject bottom-[0%]` : `hideProject bottom-[-74%]`} overflow-card bg-slate-900/80 backdrop-blur-sm  left-0 w-[100%] h-[100%]`}>
-        <div className={`project title flex justify-between md:justify-center px-5 ${project.cardOpen ? `bg-slate-900/70 showProject` : ` bg-slate-900/90 hideProject`} items-center  opacity-80`}>
-            <h2 className=" py-1 text-[1.5rem] md:py-3  md:text-[1.8rem] font-bold text-sky-300  intro-name">{project.name}</h2>
+        <div className={`project title flex justify-between md:justify-center px-5 ${project.cardOpen ? `bg-slate-900 showProject` : ` bg-slate-900/90 hideProject`} items-center  opacity-80`}>
+            <h2 className=" py-1 text-[1.3rem] md:py-3  md:text-[1.7rem] font-bold text-sky-300  intro-name">{project.name}</h2>
               {!project.cardOpen ? (
                  <button type="button" className="up-arrow-container" onClick={handleClick}>
                   <IoIosArrowUp className={`text-lg  text-gray-50 md:hidden `}/>
@@ -50,7 +50,7 @@ const ProjectCard = ({ project, handlePopUp, openPopCard }) => {
               )}
 
         </div>
-        <ul className="project-body flex  flex-wrap justify-evenly items-center py-5 ">
+        <ul className={`project-body flex ${!project.cardOpen && !isHovered && ' bg-slate-900/70  transition-all'} flex-wrap justify-evenly items-center py-5`} >
             {project.techStack.map((tech, id) => (
               <li key={id} className="text-sm md:text-lg text-sky-300 px-1 intro-name font-bold  ">{tech}</li>
             ))}
